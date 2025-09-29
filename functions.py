703f3d7e0beef676037317690d40a632a58d35c3
Laman <gilbert.laman@primergrp.com>
2025-09-29 15:53:48 +0800

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,137 @@
+from google.cloud import secretmanager
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import os
+import json
+import uuid
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_url = 'https://us-west1-pgc-dma-dev-sandbox.cloudfunctions.net/cash-non-cash-gemini-test'
+
+def get_credentials_from_secret_manager(project_id: str, secret_id: str, scopes: list = None):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    service_account_info = json.loads(response.payload.data.decode("UTF-8"))
+
+    if scopes:
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info, scopes=scopes
+        )
+    else:
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info
+        )
+
+    return credentials
+
+def get_drive_service():
+    creds = get_credentials_from_secret_manager(
+        os.getenv("BQ_PROJECT_NAME"), 
+        os.getenv("GDRIVE_FOLDER_SECRET_FROM_SECRET_MANAGER"), 
+        os.getenv("SCOPE"))
+    service = build('drive', 'v3', credentials=creds)
+    return service
+
+BQ_WRITER_CREDENTIALS = get_credentials_from_secret_manager(
+    os.getenv("BQ_PROJECT_NAME"), 
+    os.getenv("BQ_DATA_WRITER")
+)
+
+BQ_READER_CREDENTIALS = get_credentials_from_secret_manager(
+    os.getenv("BQ_PROJECT_NAME"),
+    os.getenv("BQ_DATA_READER")
+)
+
+API_CREDENTIALS = get_credentials_from_secret_manager(
+    os.getenv("BQ_PROJECT_NAME"),
+    os.getenv("API_SECRET_ID_FROM_SECRET_MANAGER")
+)
+
+BQ_CLIENT_WRITER = bigquery.Client(
+    credentials=BQ_WRITER_CREDENTIALS, 
+    project=BQ_WRITER_CREDENTIALS.project_id
+)
+
+def is_valid_api_key(api_key):
+    api_client = bigquery.Client(
+        credentials=API_CREDENTIALS, project=API_CREDENTIALS.project_id)
+
+    query = f"""
+        SELECT 1 FROM `{os.getenv("API_PROJECT_TABLE_NAME")}`
+        WHERE api_key = @api_key
+        AND active = 1
+        LIMIT 1
+    """
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("api_key", "STRING", api_key)
+        ]
+    )
+    results = api_client.query(query, job_config=job_config).result()
+    return any(results)
+    
+def save_file_info_to_bq(orig_file_name, file_name, ir_type, ir_description, uploaded_by):
+    try:
+        file_id = str(uuid.uuid4())
+
+        query = f"""
+            insert into `{os.getenv("BQ_PROJECT_NAME")}.cash_non_cash.store_upload_master` (
+                    file_id,
+                    file_original_name,
+                    file_new_name,
+                    date_uploaded,
+                    uploaded_by,
+                    ir_type,
+                    ir_description
+                )
+                values
+                (
+                    @file_id,
+                    @file_name,
+                    @new_file_name,
+                    CURRENT_DATETIME(),
+                    @uploaded_by,
+                    @ir_type,
+                    @ir_description
+                )
+            """
+        query_parameters = [
+            bigquery.ScalarQueryParameter("file_id", "STRING", file_id),
+            bigquery.ScalarQueryParameter(
+                "file_name", "STRING", orig_file_name),
+            bigquery.ScalarQueryParameter(
+                "new_file_name", "STRING", file_name),
+            bigquery.ScalarQueryParameter("uploaded_by", "STRING", uploaded_by),
+            bigquery.ScalarQueryParameter("ir_type", "STRING", ir_type),
+            bigquery.ScalarQueryParameter("ir_description", "STRING", ir_description)
+        ]
+
+        job_config = bigquery.QueryJobConfig(query_parameters=query_parameters)
+
+        query_job = BQ_CLIENT_WRITER.query(query, job_config=job_config)
+
+        query_job.result()
+        
+        print("Successfully save to BQ.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+def gemini_processing(filepath, new_file_name):
+    try:
+        print(f"Gemini processing has started for file: {filepath}")
+        with open(filepath, "rb") as f:
+            files = {"file": (new_file_name, f)}
+            requests.post(gemini_api_url, files=files)
+
+        # return {'status':'success', 'message':'Sent to gemini for processing.'}
+
+    except Exception as e:
+        print(f"Gemini processing error: {e}")
+        # return {"status": False, "message": str(e)}
+    
---
+++ main.py
@@ -0,0 +1,170 @@
+from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
+import uuid
+from functions import save_file_info_to_bq, get_drive_service, is_valid_api_key, gemini_processing
+
+load_dotenv()
+
+app = Flask(__name__)
+
+TEMP_FOLDER=os.getenv("TEMP_FOLDER")
+
+os.makedirs(TEMP_FOLDER, exist_ok=True)
+app.config["TEMP_FOLDER"] = TEMP_FOLDER
+
+@app.route('/upload_ir', methods=['POST'])
+def upload():
+    try:
+        api_key = request.headers.get('X-API-Key')
+        if not api_key or not is_valid_api_key(api_key):
+            return jsonify({"status": "error",
+                            "message": "Unauthorized. Invalid API key."}), 401
+
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part in the request.'}), 400
+
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No file selected.'}), 400
+
+        ir_type = request.form.get('ir_type')
+        ir_description = request.form.get('ir_description')
+        uploaded_by = request.form.get('uploaded_by')
+
+        # Generate unique filename
+        name, ext = os.path.splitext(file.filename)
+        uid = str(uuid.uuid4())
+        parts = uid.split('-')
+        collected_parts = [p[:4] for p in parts]
+        short_parts = ("-".join(collected_parts))
+        new_file_name = f"{name}-{short_parts}{ext}"
+
+        filename = secure_filename(new_file_name)
+        filepath = os.path.join(app.config['TEMP_FOLDER'], filename)
+        file.save(filepath)
+
+        # Step 2: Save info to BQ
+        save_file_info_to_bq(file.filename, filename, ir_type, ir_description, uploaded_by)
+
+        # Step 3: Upload to Drive
+        service = get_drive_service()
+        file_metadata = {
+            'name': filename,
+            'parents': [os.getenv("UPLOAD_FOLDER_ID")]
+        }
+        media = MediaFileUpload(filepath, resumable=True)
+
+        try:
+            uploaded_file = service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id, name, webViewLink',
+                supportsAllDrives=True
+            ).execute()
+        finally:
+            if media._fd:
+                media._fd.close()
+                
+        # Step 4: Gemini processing
+        gemini_processing(filepath,filename)
+
+        # Step 5: Cleanup
+        try:
+            if os.path.exists(filepath):
+                os.remove(filepath)
+                print(f"Deleted: {filepath}")
+                delete_status = f"Deleted: {filepath}"
+            else:
+                print(f"File not found for cleanup: {filepath}")
+        except Exception as cleanup_err:
+            print(f"Cleanup failed: {cleanup_err}")
+            delete_status = f"Cleanup failed: {cleanup_err}"
+
+        return jsonify(
+            {'message': f'{uploaded_file["name"]}',
+            'cleanup_status': delete_status}
+        ), 200
+
+    except Exception as e:
+        print(f"Error uploading file: {str(e)}")
+        return jsonify({'message': f"Error uploading file: {str(e)}"}), 500
+    
+
+@app.route('/upload_bulk_to_gdrive', methods=['POST'])
+def upload_bulk_to_gdrive():
+    try:
+        api_key = request.headers.get('X-API-Key')
+        if not api_key or not is_valid_api_key(api_key):
+            return jsonify({"status": "error",
+                            "message": "Unauthorized. Invalid API key."}), 401
+            
+        print("starting bulk gdrive upload...")
+        if 'bulk_file' not in request.files:
+            return jsonify({'error': 'No file part in the request.'}), 400
+
+        files = request.files.getlist('bulk_file')
+        uploaded_by = request.form.get('uploaded_by')
+
+        print(f"Uploaded By: {uploaded_by}")
+        
+        for file in files:
+            filename = file.filename
+            filepath = os.path.join(app.config['TEMP_FOLDER'], filename)
+            print(f"Filename: {filename}")
+
+            name, ext = os.path.splitext(filename)
+            uid = str(uuid.uuid4())
+            parts = uid.split('-')
+            collected_parts = [p[:4] for p in parts]
+            short_parts = ("-".join(collected_parts))
+            new_created_file_name = f"{name}-{short_parts}{ext}"
+            
+            new_file_name = secure_filename(new_created_file_name)
+            
+            file.save(filepath)
+            
+            service = get_drive_service()
+            file_metadata = {
+                'name': new_file_name,
+                'parents': [os.getenv("UPLOAD_FOLDER_ID")]
+            }
+            media = MediaFileUpload(filepath, resumable=True)
+            
+            try:
+                service.files().create(
+                    body=file_metadata,
+                    media_body=media,
+                    fields='id, name, webViewLink',
+                    supportsAllDrives=True
+                ).execute()
+            finally:
+                save_file_info_to_bq(filename,new_file_name,"test","test description",uploaded_by)
+                gemini_processing(filepath, new_file_name)
+                if media._fd:
+                    media._fd.close()
+                    
+            try:
+                if os.path.exists(filepath):
+                    os.remove(filepath)
+            except Exception as cleanup_err:
+                print(f"Cleanup failed: {cleanup_err}")
+                
+        return jsonify(
+            {
+                'message': 'Upload complete.',
+                'status': 'success'
+            }
+        ),200
+                   
+    except Exception as e:
+        return jsonify({"status": "failed"}), 500
+
+
+# if __name__ == '__main__':
+#     from googleapiclient.http import MediaFileUpload
+#     app.run(debug=True, port=5001)
+
+if __name__ == '__main__':
+    app.run(debug=True)
